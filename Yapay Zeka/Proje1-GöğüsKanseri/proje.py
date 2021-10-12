0f2c976aabecc4360435ca2a9ba3ab0a2c6bca63
Özgür Tokay <ozytky@gmail.com>
2021-10-12 23:44:26 +0300

    Exploratory Data Science

--- Yapay Zeka/Proje1-GöğüsKanseri/proje.py
+++ Yapay Zeka/Proje1-GöğüsKanseri/proje.py
@@ -54,8 +54,53 @@ standardization
 missin value:none //yani kayıp değerler bu veride yok
 """
 
+# %% EDA 
+
+#Correlation / Korelasyon
+corr_matrix= data.corr()
+sns.clustermap(corr_matrix, annot=True,  fmt=".2f")
+plt.title("Correlation Between Features")
+plt.show()
 
 
+#
+threshold = 0.75 # bu değerden yüksek olanları filtrelemek
+filtre = np.abs(corr_matrix["target"]) > threshold
+corr_features = corr_matrix.columns[filtre].tolist()
+sns.clustermap(data[corr_features].corr() , annot=True, fmt=".2f")  
+plt.title("Correlation Between Features with Corr Threshold 0.75")
+
+"""
+there are some  corrleted featrues
+"""
+
+
+#box plot
+data_melted=pd.melt(data, id_vars="target",
+                    var_name="features",
+                    value_name="value")
+plt.figure()#yeni figür oluştur
+sns.boxplot(x = "features", y="value", hue="target", data=data_melted)
+plt.xticks(rotation=90)#featuresların isimleri 90 derece dik olucaktır 
+plt.show()
+
+"""
+Buradaki box_plottan anlaam çıkartabilmemiz için veri normalizasyonaa yada standardization'a
+ tabi tutmamız lazım
+"""
+
+"""
+standardization-normalization
+"""
+
+#pair plot
+sns.pairplot(data[corr_features], diag_kind="kde", markers="+", hue="target")
+plt.show()
+
+
+"""
+skewness (çarpıklık)
+"""
 
 
 
